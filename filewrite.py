45b8cac5ff3778fc3fbaa0dd582299883ccf38cb
GabrielTheophilo <gabrieltsf10@gmail.com>
2021-08-12 09:35:46 -0300

    Modified filewriting to be more organized and provide a unique folder to save all research terms

--- filewrite.py
+++ filewrite.py
@@ -1,27 +1,58 @@
 import os
 import os.path
+
+class FolderWrite:
+    def FolderWrite():
+        username = os.getlogin()
+        try:
+            os.mkdir(f'C:\\Users\\{username}\\Desktop\\NewsApiScript')
+            return 0
+            
+        except:
+            print("Pasta já existente na área de trabalho")
+            return 1
+
 class FileWrite:
     def OpenFileWriteJson(str,*args):
         username = os.getlogin()
-
+        
+        folder = FolderWrite.FolderWrite()
+        if folder == 0:
+            path = (f'C:\\Users\\{username}\\Desktop\\NewsApiScript')
+        if folder == 1:
+            try:
+                path = (f'C:\\Users\\{username}\\Desktop\\NewsApiScript')
+            except:
+                path = (f'C:\\Users\\{username}\\Desktop')
+        
         try:
-            file = open(f'C:\\Users\\{username}\\Desktop\\{str}.json', 'x')
+            file = open(f'{path}\\{str}.json', 'x')
             file.write(*args)
             file.close()
 
         except:
-            file = open(f'C:\\Users\\{username}\\Desktop\\{str}.json', 'a')
+            file = open(f'{path}\\{str}.json', 'a')
             file.write(*args)
             file.close()
 
     def OpenFileWriteTxt(str,*args):
         username = os.getlogin()
+        
+        folder = FolderWrite.FolderWrite()
+        if folder == 0:
+            path = (f'C:\\Users\\{username}\\Desktop\\NewsApiScript')
+        if folder == 1:
+            try:
+                path = (f'C:\\Users\\{username}\\Desktop\\NewsApiScript')
+            except:
+                path = (f'C:\\Users\\{username}\\Desktop')
+        
         try:
-            file = open(f'C:\\Users\\{username}\\Desktop\\{str}.txt', 'x')
+            file = open(f'{path}\\{str}.txt', 'x')
             file.write(*args)
             file.close()
 
         except:
-            file = open(f'C:\\Users\\{username}\\Desktop\\{str}.txt', 'a')
+            file = open(f'{path}\\{str}.txt', 'a')
             file.write(*args)
             file.close()
